0f45d42a33b6fda5a6cf47b803c5f17ac0f897b1
YasasriBanuka <banukadias5@gmail.com>
2026-09-16 19:50:55 +0530

    add login credential data

--- utils/testData.ts
+++ utils/testData.ts
@@ -1,9 +1,16 @@
 
-// Registration form data
+// Registration form data for user
 export const registrationData = {
     firstName: 'Banuka',
     lastName: 'Dias',
-    email: 'banukadias5+yasasri123@gmail.com',
+    email: 'banukadias5+yasa12@gmail.com',
     password: process.env.REGISTER_PASSWORD!,
     branchId: process.env.REGISTER_BRANCH_ID!
 };
+
+// super admin login data 
+export const adminLoginData = {
+    email: 'admin.primary@companydemo.com',
+    password: process.env.REGISTER_ADMIN_PASSWORD!,
+    branchId: process.env.REGISTER_BRANCH_ID!
+}
